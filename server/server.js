fcc81ff27d3be6e4b3eea59fd5a167ce52b5ecfb
Chain Choonoi <chain777@hotmail.com>
2016-04-05 15:47:56 +0700

    delay at start

--- server/server.js
+++ server/server.js
@@ -20,9 +20,12 @@ app.start = function() {
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+  console.log('Welcome to My Console,');
+  setTimeout(function() {
+    if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
+    // start the server if `$ node server.js`
+    if (require.main === module)
     app.start();
+  }, 10000);
 });
