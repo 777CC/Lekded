d5e900a42983040b23a0d0454c3844b119f8a458
Chain Choonoi <chain777@hotmail.com>
2016-04-05 11:22:50 +0700

    Change port to process.env.PORT

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log(process.env.PORT);
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
